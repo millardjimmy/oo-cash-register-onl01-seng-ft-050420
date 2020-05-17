1c2cfca44fef7d8e04008bf5a6087cea3b07bd51
Jimmy Millard <millardjimmy@gmail.com>
2020-05-17 23:18:03 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -10,6 +10,7 @@ class CashRegister
   def add_item(item, price, quantity = 1)
     @price = price
     @total += price * quantity
+    @last_transaction_amount = @total
     if quantity > 1
       counter = 0
       while counter < quantity
@@ -32,7 +33,7 @@ class CashRegister
   end
 
   def void_last_transaction
-    @total -= @price
+    @total -= @last_transaction_amount
   end
 
 end
